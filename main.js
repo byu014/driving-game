d97209e168715c28791056b0693e42b11b1e354c
Bailey Yu <bailey.yu@yahoo.com>
2021-11-04 13:32:52 -0700

    user can now turn car

--- main.js
+++ main.js
@@ -4,11 +4,22 @@ const directions = {
   ArrowRight: 'east',
   ArrowLeft: 'west'
 };
+
+const turns = {
+  east: 0.0,
+  south: 0.25,
+  west: 0.5,
+  north: 0.75
+};
 const car = {
   direction: 'east'
 };
 
+let turn = 0.0;
+
 const $img = document.querySelector('img');
 
 window.addEventListener('keydown', function (event) {
+  car.direction = directions[event.key];
+  $img.style.transform = `rotate(${turns[car.direction]}turn)`;
 });
